8bcc34cf967ce1dfea454c912af034683b9739ec
Daniel Lucas <daniel.chris.lucas@gmail.com>
2015-01-06 21:36:56 +0100

    Added slug autocompletion

--- blogengine/admin.py
+++ blogengine/admin.py
@@ -1,7 +1,8 @@
 import blogengine.models
 from django.contrib import admin
 
+
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title", ), }
 
-admin.site.register(blogengine.models.Post)
+admin.site.register(blogengine.models.Post, PostAdmin)
